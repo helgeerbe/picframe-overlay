6bee02fa25f0aa896ddc788e6b1664a4ada3c947
helgeerbe <helge@erbehome.de>
2021-04-18 23:24:44 +0200

    first proof of concept

--- README.md
+++ README.md
@@ -11,6 +11,8 @@ picframe-overlay uses [electron](https://www.electronjs.org). Follow [quick-star
 ### Raspbery
 On raspi you need to run a composite manager to get a transparent window. By default it's `xcompmgr`. Unfortuantely activation of `xcompmgr` over `raspi-config` is not working. Instead append `@xcompmgr` in file `/etc/xdg/lxsession/LXDE-pi/autostart`. This will start `xcompmgr`when user `pi`is logged in to the desktop.
 
+You have to enable fake KMS GL driver in `raspi-config`
+
 ## Installation
 
 ````bash
@@ -20,6 +22,17 @@ $ git clone https://github.com/helgeerbe/picframe-overlay
 # Go into the repository
 $ cd picframe-overlay
 
+# create .env file
+WEATHER_KEY=get key from https://openweathermap.org/
+PICFRAME_URL=http://my_picframe:9000/?all
+
 # Install the dependencies and run
 $ npm install && npm start
 ````
+
+## How to use
+
+set fade_time to
+- 10 empty overlay
+- 11 digital clock
+- 12 weather 
--- main.js
+++ main.js
@@ -1,15 +1,20 @@
-const { app, BrowserWindow, globalShortcut } = require('electron')
+// Modules to control application life and create native browser window
+const { app, BrowserWindow, ipcMain  } = require('electron')
 const path = require('path')
 const fetch = require('electron-fetch').default
+const config = require('dotenv').config();
 
-let mainWindow = null;
+console.log(`Your PictureFrame url is: ${process.env.PICFRAME_URL}`); 
 
-function onAppReady() {
+var actual_overlay = -1;
+var mainWindow = null;
 
-  mainWindow = new BrowserWindow({
+function createWindow () {
+  // Create the browser window.
+    mainWindow = new BrowserWindow({
     width: 800,
     height: 600,
-    frame: false,
+    frame: false, // no window decorations
     transparent: true,
     alwaysOnTop: true,
     autoHideMenuBar: true,
@@ -20,39 +25,69 @@ function onAppReady() {
     }
   });
 
+  loadOverlay()
 
-
-
-  fetch('https://picframe.erbehome.de:9000/?fade_time')  
-    .then(res => res.json())
-    .then(function(data) {
-      if (data["fade_time"] == 10) {
-        mainWindow.loadFile('clock.html');
-        console.log(data);
-      }
-      else {
-        mainWindow.loadFile('weather.html');
-      }
-    })
-    .catch(function() {
-      console.log("Can't connect to PictureFrame webserver.");
-    });
-
-
-  
-
-  const shortcut = globalShortcut.register('Control+Space', () => {
-    mainWindow.show();
-  });
-
-  if (!shortcut) { console.log('Registration failed.'); }
-
-  mainWindow.on('close', (event) => {
-    event.preventDefault();
-    mainWindow.hide();
-  });
+  //mainWindow.webContents.openDevTools()
 }
 
 
+// This method will be called when Electron has finished
+// initialization and is ready to create browser windows.
+// Some APIs can only be used after this event occurs.
+app.whenReady().then(() => {
+  setTimeout(createWindow, 500); // ther seems to be a timing issue for transparent windows.
+  
+  app.on('activate', function () {
+    // On macOS it's common to re-create a window in the app when the
+    // dock icon is clicked and there are no other windows open.
+    if (BrowserWindow.getAllWindows().length === 0) setTimeout(createWindow, 500)
+  })
+})
 
-app.on('ready', () => setTimeout(onAppReady, 500));
+// Quit when all windows are closed, except on macOS. There, it's common
+// for applications and their menu bar to stay active until the user quits
+// explicitly with Cmd + Q.
+app.on('window-all-closed', function () {
+  if (process.platform !== 'darwin') app.quit()
+})
+
+
+// In this file you can include the rest of your app's specific main process
+// code. You can also put them in separate files and require them here.
+
+function loadOverlay() {
+  // load in picframe selected overlay
+  fetch(process.env.PICFRAME_URL)  
+  .then(res => res.json())
+  .then(function(data) {
+    if (actual_overlay != data["fade_time"] ) {
+      actual_overlay = data["fade_time"];
+      console.log('Actual selected overlay is %d.', actual_overlay);
+      switch (actual_overlay) {
+        case 11:
+          console.log("Load clock overlay");
+          mainWindow.loadFile('clock.html');
+          break;
+        case 12:
+          console.log("Load weather overlay");
+          mainWindow.loadFile('weather.html');
+          break;
+        default:
+          console.log("Load empty overlay");
+          mainWindow.loadFile('empty.html');
+      }
+    }
+  })
+  .catch(function() {
+    console.log("Can't connect to PictureFrame webserver.");
+  });
+
+  // reload Overlay
+  setTimeout(loadOverlay, 1000)
+}
+
+// send environment vars to renderer
+ipcMain.on("toMain", (event, args) => {
+  console.log(`Recieved: ${args}`);
+    mainWindow.webContents.send("fromMain", config.parsed);
+});
