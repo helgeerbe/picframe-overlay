f93ff14ebf3c886f144491e3646b10a7e671fe24
helgeerbe <helge@erbehome.de>
2021-04-21 01:08:07 +0200

    current weather

--- weather.js
+++ weather.js
@@ -3,7 +3,7 @@
  * Fetch current weather data from openweathermap.org
  * Initiate page update
  *
- * @param {json} data - Querry data as json object {"city": "myCity", "lat": 52.38, "long": 13.21, "key": "mykey", "lang": "de", "units": "metric"}
+ * @param {json} data - Querry data as json object {"city": "myCity", "lat": 52.38, "lon": 13.21, "key": "mykey", "lang": "de", "units": "metric"}
  * @return {void} Nothing
  */
 function weatherBallon( data ) {
@@ -28,10 +28,21 @@ function weatherBallon( data ) {
 function drawWeather( city, d ) {
     let temp = Math.round(parseFloat(d.current.temp));
     let description = d.current.weather[0].description; 
+    let icon = `https://openweathermap.org/img/wn/${d.current.weather[0]["icon"]}@2x.png`;
+    var date = new Date(d.current.dt * 1000);
 
+    document.getElementById('icon').src = icon
     document.getElementById('description').innerHTML = description;
     document.getElementById('temp').innerHTML = temp + '&deg;';
     document.getElementById('location').innerHTML = city;
+    document.getElementById('pressure').innerHTML = d.current.pressure + " hPa";
+    document.getElementById('humidity').innerHTML = d.current.humidity + "%";
+    document.getElementById('date').innerHTML = `${date.toLocaleDateString('de-DE')} ${date.toLocaleTimeString('de-DE')}`;
+
+    for (i = 0; i < 7; i++) {
+      var date = new Date(d.daily[i].dt * 1000);
+      document.getElementById('daily'+i).innerHTML = `${date.toLocaleDateString('de-DE')}`;
+  }
 }
 
 /**
