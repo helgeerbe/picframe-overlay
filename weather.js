dac0f7ee1f72e5d1d5fc8eb8dff20f8b9e30b6de
helge <helge@erbehome.de>
2021-04-21 17:40:50 +0200

    final weather layout

--- weather.js
+++ weather.js
@@ -1,3 +1,4 @@
+var config;
 
 /**
  * Fetch current weather data from openweathermap.org
@@ -16,6 +17,7 @@ function weatherBallon( data ) {
 	.catch(function() {
 		// catch any errors
 	});
+  //setTimeout(function(){weatherBallon(config);},900000);
 }
 
 /**
@@ -27,21 +29,39 @@ function weatherBallon( data ) {
  */
 function drawWeather( city, d ) {
     let temp = Math.round(parseFloat(d.current.temp));
+    let tempMin = Math.round(parseFloat(d.daily[0].temp.min));
+    let tempMax = Math.round(parseFloat(d.daily[0].temp.max));
     let description = d.current.weather[0].description; 
     let icon = `https://openweathermap.org/img/wn/${d.current.weather[0]["icon"]}@2x.png`;
     var date = new Date(d.current.dt * 1000);
+    var sunrise = new Date(d.current.sunrise *1000);
+    var sunset = new Date(d.current.sunset *1000);
 
     document.getElementById('icon').src = icon
     document.getElementById('description').innerHTML = description;
     document.getElementById('temp').innerHTML = temp + '&deg;';
+    document.getElementById('tempMinMax').innerHTML = `${tempMax}&deg; <i class="mdi mdi-thermometer-plus"></i>  ${tempMin}&deg;  <i class="mdi mdi-thermometer-minus"</i>`;
     document.getElementById('location').innerHTML = city;
-    document.getElementById('pressure').innerHTML = d.current.pressure + " hPa";
-    document.getElementById('humidity').innerHTML = d.current.humidity + "%";
+    document.getElementById('pressure').innerHTML = `<i class="mdi mdi-gauge"></i> ${d.current.pressure}h/Pa`;
+    document.getElementById('humidity').innerHTML = `<i class="mdi mdi-water-percent"></i> ${d.current.humidity}%`;
+    document.getElementById('wind').innerHTML = `<i class="mdi mdi-weather-windy"></i> ${d.daily[0].wind_speed}(${d.daily[0].wind_gust})km/h`;
+    document.getElementById('sunrise').innerHTML = `<i class="mdi mdi-weather-sunset-up"></i> ${sunrise.toLocaleTimeString('de-DE')}`;
+    document.getElementById('sunset').innerHTML = `<i class="mdi mdi-weather-sunset-down"></i> ${sunset.toLocaleTimeString('de-DE')}`;
     document.getElementById('date').innerHTML = `${date.toLocaleDateString('de-DE')} ${date.toLocaleTimeString('de-DE')}`;
 
-    for (i = 0; i < 7; i++) {
+    var days = ['Sonntag', 'Montag', 'Dienstag', 'Mittwoch', 'Donnerstag', 'Freitag', 'Samstag'];
+    let elementName = null;
+    for (i = 1; i < 8; i++) {
       var date = new Date(d.daily[i].dt * 1000);
-      document.getElementById('daily'+i).innerHTML = `${date.toLocaleDateString('de-DE')}`;
+      let forecast = `<ul class="daily_list">`;
+      forecast += `<li>${days[date.getDay()]}</li>`;
+      forecast += `<li>${Math.round(parseFloat(d.daily[i].temp.max))}&deg; <i class="mdi mdi-thermometer-plus"></i></li>`;
+      forecast += `<li><img class="img_daily"; src="https://openweathermap.org/img/wn/${d.daily[i].weather[0]["icon"]}@2x.png"></li>`;
+      forecast += `<li class="description">${d.daily[i].weather[0].description}</li>`;
+      forecast += `<li>${Math.round(parseFloat(d.daily[i].temp.min))}&deg; <i class="mdi mdi-thermometer-minus"></i></li>`;
+      forecast += "</ul>";
+      elementName = 'daily'+i
+      document.getElementById(elementName).innerHTML = forecast;
   }
 }
 
@@ -49,7 +69,7 @@ function drawWeather( city, d ) {
  * On window load, read config and fetch current weather data
  */
 window.onload = function() {
-  const config = JSON.parse(window.api.readConfig().WEATHER);
+  config = JSON.parse(window.api.readConfig().WEATHER);
   console.log(`Read city  ${config.lat}/${config.lon} and key ${config.key} from .env file` )
   weatherBallon( config );
 }
